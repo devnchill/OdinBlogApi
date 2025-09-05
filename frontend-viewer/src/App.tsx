a349b15db29e8fe6ee1138aa722a6ca58e36f7b3
0xDev <169875752+devnchill@users.noreply.github.com>
2025-09-05 23:06:03 +0530

    feat: wrote signup and login component

--- frontend-viewer/src/App.tsx
+++ frontend-viewer/src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import SignUp from "./pages/signup";
+import Login from "./pages/login";
+
 function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
